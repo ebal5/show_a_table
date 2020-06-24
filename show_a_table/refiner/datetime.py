a16f19c2c43d3571b00689e86acfdf773e12c5de
eval <eval.scheme@gmail.com>
2020-06-25 07:26:00 +0900

    add init py

---
+++ show_a_table/refiner/datetime.py
@@ -0,0 +1,19 @@
+"""Refines date and time.
+
+1. selection of year. (This year, last year, past(unit = thousand? hundred?))
+2. selection of month of year.
+3. selection of day of week
+4. selection of date of month.
+"""
+
+import fire
+import re
+
+
+class DateTimeRefiner:
+    """Refines date and time. """
+    def __init__(self):
+        pass
+
+    def refine(self):
+        pass

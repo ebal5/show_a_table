16cce7a16bb9b4d62364c3d04a67368d21609943
eval <eval.scheme@gmail.com>
2020-06-25 20:21:39 +0900

    add refiner

---
+++ show_a_table/refiner/refiner.py
@@ -0,0 +1,13 @@
+"""definition of basic refiner"""
+
+
+class Refiner:
+    """default refiner
+
+    used by not specific ENE's
+    """
+    def __init__(self, ene: str):
+        pass
+
+    def get_query(self, state):
+        pass

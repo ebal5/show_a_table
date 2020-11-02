109117ed89dac50d8e082226ae1c8da7df932a6a
eval <eval.scheme@gmail.com>
2020-11-03 02:52:30 +0900

    update util(add dic_to_seq_of_cands, KanaRefiner)

--- show_a_table/refiner/util.py
+++ show_a_table/refiner/util.py
@@ -1,8 +1,9 @@
 from itertools import groupby
-from operator import itemgetter
 from importlib.resources import read_text
 import toml
 import pykakasi
+
+from .refiner import Refiner
 
 
 def _gbh(lst, key=0, idx=0):
@@ -62,11 +63,48 @@ def make_n_dict(_list, max_cands=0):
     kks = pykakasi.kakasi()
     lst = [(s, l, [d["kana"] for d in kks.convert(l)].join("")) for s, l in _list]
     dic = _gbh(lst, 2, 0)
-    checked = [k for k, v in dic.items() if len(v) >= 20]
+    checked = [k for k, v in dic.items() if len(v) > max_cands]
     idx = 0
     while checked:
         for k in checked:
             dic = _merge_dict(dic, _gbh(dic[k], key=2, idx=idx), k)
-        checked = [k for k, v in dic.items() if len(v) >= 20]
+        checked = [k for k, v in dic.items() if len(v) > max_cands]
         idx += 1
     return dic
+
+
+def dic_to_seq_of_cands(dic, max_cands=0):
+    """
+    make_n_dictで出力される形式から候補リストのシーケンスを返す
+
+    もし，全候補数がmax_cands以下ならば全候補をまとめたリストを，
+    そうでなくdicのキー数が20以下ならば全てのキーを，
+    そうでないならばdicのキーを辞書順ソートしたものをmax_candsずつに区切る
+
+    Parameters
+    ----------
+    dic: Dict[str, List[Tuple[str, str, str]]] カナをキーとした，(主語，ラベル，カナ)のリスト
+    max_cands: int 最大候補数．0ならば設定から取得
+
+    Returns
+    -------
+    """
+    if max_cands == 0:
+        max_cands = toml.loads(read_text(__package__, "config.toml"))["max_cands"]
+        pass
+    whole_len = sum([len(v) for v in dic.values()])
+    if whole_len <= max_cands:
+        # 全候補を返す
+        pass
+    elif len(dic.keys()) <= max_cands:
+        # 全キーと結びつきを返す
+        pass
+    else:
+        # 全キーを順に返す
+        pass
+
+
+class KanaRefiner(Refiner):
+    """
+    50音をカタカナで選択するためだけのAPI
+    """

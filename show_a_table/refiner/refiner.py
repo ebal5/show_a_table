d183e932168f5c3f75e56afd1153d5b452f75db9
eval <eval.scheme@gmail.com>
2020-11-03 02:57:32 +0900

    add blank classes for candidates

--- show_a_table/refiner/refiner.py
+++ show_a_table/refiner/refiner.py
@@ -8,3 +8,19 @@ class Refiner:
 
     def refine(choice: str = "") -> Tuple[bool, List[str]]:
         return (False, [])
+
+
+class Candidate:
+    """
+    Refinerが返す候補の個別要素を示すクラス．
+    典型的には表示値とRefinerに戻す値は同じだが，SPARQLを利用するなどでかわる可能性がある
+    """
+    pass
+
+
+class Candidates:
+    """
+    Refinerが返す候補一覧を保持するクラス
+    あとついでにそのRefinerの責任範囲が終了したことを示す信号も持つ．
+    """
+    pass

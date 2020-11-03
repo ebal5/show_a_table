b83c39d3ee69b560076a680601b018061ef0b90d
eval <eval.scheme@gmail.com>
2020-11-04 08:28:28 +0900

    Queryクラスの追加 (#7)
    * add DQQuery class
    
    * add specific query class
    
    Co-authored-by: Shuichi Tani <muminn4@gmail.com>

--- show_a_table/model/refiner/refiner.py
+++ show_a_table/model/refiner/refiner.py
@@ -110,3 +110,99 @@ class Candidates(list):
         """
         del self[:num_cands]
         return self
+
+
+class DQQuery:
+    """
+    DrQAに回すクエリを保持するクラス．
+
+    これ自体は基底クラスとしての使用に留まり，実際は派生クラスが使われるはず
+    """
+
+    def exam(self, result):
+        """
+        Parameters
+        ----------
+        result : str
+          QAシステムの戻り値．SGMLタグを含まないテキストを期待する
+
+        Returns
+        -------
+        is_passed
+          `result` がこの試験を通過可能か示すブール値
+        """
+        return False
+
+    def make_query(self, target):
+        """
+        Parameters
+        ----------
+        target : str
+          Wikipedia 記事タイトルになることが多いかと
+          QAシステムに投げる質問文を生成する
+
+        Returns
+        -------
+        str
+          生成された質問文
+        """
+        return ""
+
+
+class RegQuery(DQQuery):
+    """
+    正規表現による絞りを実施するクラス
+
+    Attributes
+    ----------
+    reg : re
+      絞りのための正規表現オブジェクト
+    """
+
+    def __init__(self, regex, gen_query):
+        """
+        Parameters
+        ----------
+        regex : re
+          絞りのための正規表現オブジェクト．
+          グループ化してもよいが，特別使用する方法は用意しない
+        gen_query : str -> str
+          質問生成のための関数
+        """
+        self.reg = regex
+        self.gen = gen_query
+
+    def exam(self, result):
+        return True if self.reg.match(result) else False
+
+    def get_query(self, target):
+        return self.gen(target)
+
+
+class FunQuery(DQQuery):
+    """
+    独自実装の関数による絞りを実施するクラス
+
+    Attributes
+    ----------
+    refine : str -> bool
+    gen : str -> str
+    """
+
+    def __init__(self, refine, gen_query):
+        """
+        Parameters
+        ----------
+        refine : str -> bool
+          絞りを実施する関数．bool値を返す
+        gen_query : str -> str
+          質問文を生成する関数
+        """
+        self.refine = refine
+        self.gen = gen_query
+
+    def exam(self, result):
+        return self.refine(result)
+
+    def get_query(self, target):
+        return self.gen(target)

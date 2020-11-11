37d6a53cb21cf070058fd183dd88a7c16f0fb3a8
eval <eval.scheme@gmail.com>
2020-11-11 10:22:45 +0900

    change to github-flow (#19)
    * implement of candidates (#15)
    
    * add date refiner(not perfectly implemented)
    
    * add util to minimize the size of candidates
    
    * add Class KanaCandidates
    
    * remove unused files
    
    * implement KanaCanddidates
    
    * implement geo refiner (#16)
    
    * implement category_selector (#17)
    
    * implementation of CandsPerformer
    
    * 修正完了． (#18)

---
+++ show_a_table/model/refiner/category_selector.py
@@ -0,0 +1,79 @@
+from importlib.resources import read_text
+
+import toml
+
+from .date import DateRefiner
+from .geo_refiner import GeoRefiner
+from .refiner import Candidates, Categories
+
+
+class CategorySelector:
+    """
+    カテゴリの選択や属性の選択，Refinerの生成を行う
+
+    あるいみRootRefiner
+
+    Refiner ではなくすることで種々の制限からはずす．
+    """
+    def __init__(self):
+        self.cat = None
+        self.attr = None
+        self.queries = []
+        self._data = toml.loads(read_text(__package__, "config.toml"))
+
+    def categories(self):
+        if self.cat:
+            raise RuntimeError("既にカテゴリ選択は終了しています")
+        return Candidates("カテゴリの選択", [c.value for c in Categories], self)
+
+    def set_category(self, cat):
+        """
+        Parameters
+        ----------
+        cat : Candidate
+          確定したカテゴリ……？
+        """
+        self.cat = Categories.value_of(cat)
+
+    def attributes(self):
+        """"""
+        return list(self._data["attributes"][self.cat.name].keys())
+
+    def add_query(self, query):
+        """
+        Parameters
+        ----------
+        query : DQQuery
+        """
+        self.queries.append(query)
+
+    def refiners(self, attr):
+        """
+        Returns refiner based on attributes
+
+        Parameters
+        ----------
+        attr : str
+
+        Returns
+        -------
+        Refiner
+          attr に基づいたRefiner
+        """
+        key = self._data["attributes"][self.cat.name][attr]
+        if key == "Geo":
+            return GeoRefiner(attr)
+        elif key == "Date":
+            return DateRefiner(attr)
+        elif key == "Free":
+            # TODO FreeRefiner
+            raise NotImplementedError("Not yet implemented")
+            pass
+        elif key == "Number":
+            # TODO NumberRefiner
+            raise NotImplementedError("Not yet implemented")
+            pass
+        elif key == "Data":
+            # TODO DataSpecificRefiner
+            raise NotImplementedError("Not yet implemented")
+            pass
--- show_a_table/model/refiner/refiner.py
+++ show_a_table/model/refiner/refiner.py
@@ -1,4 +1,10 @@
+from collections import OrderedDict
 from enum import Enum, auto
+from importlib.resources import read_text
+
+import toml
+
+from .util import make_n_dict
 
 
 class RefinerState(Enum):
@@ -45,35 +51,6 @@ class Refiner:
         return Candidates("PLACE HOLDER", ["SAMPLE", "CHOICE"], self)
 
 
-class CategoryRefiner:
-    """
-    カテゴリ選択に関するRefiner
-    同時にルートRefinerとして各属性に関するQueryも保持する
-
-
-    Attributes
-    ----------
-    selected : Categories
-      選択されたカテゴリ
-    queries : DQQuery
-      実行するべきクエリのリスト
-    """
-
-    def __init__(self):
-        self.selected = None
-        self.queries = []
-
-    def refine(self, choice=""):
-        if choice:
-            cat = Categories.value_of(choice)
-            if cat:
-                self.selected = cat
-            else:
-                raise ValueError(f"{choice} は正しいカテゴリではありません")
-        else:
-            return Candidates("カテゴリの選択", self.categories, self, False)
-
-
 class Candidate:
     """
     Refinerが返す候補の個別要素を示すクラス．
@@ -96,7 +73,7 @@ class Candidate:
         return self.key
 
 
-class Candidates(list):
+class Candidates:
     """
     Refinerが返す候補一覧を保持するクラス
     あとついでにそのRefinerの責任範囲が終了したことを示す信号も持つ．
@@ -122,7 +99,7 @@ class Candidates(list):
         ----------
         title : str
           絞り込みのタイトル
-        cands : List[Candidate/str]
+
           候補値リスト．どちらかの型で統一される必要がある
 
         Raises
@@ -134,29 +111,178 @@ class Candidates(list):
             cands = [Candidate(c) for c in cands]
         if not (len(cands) == len(set([c.key for c in cands]))):
             raise ValueError("候補値に被りがあります．")
-        self.extend(cands)
+        self._expects = {c.key: c for c in cands}
+        self._lk = list(self._expects.keys())
         self.title = title
         self.parent = parent
+        self._last_idx = None
 
-    def spend(self, num_cands):
+    def cands(self, num_cands=30):
         """
-        num_cands 分の要素を既に提示したものとする
+        num_cands個以下の選択肢を返す．
+        0ならば全てを返す．
 
         Parameters
         ----------
         num_cands : int
-          消費した数．
 
         Returns
         -------
-        self : Candidates
+        List[str]
+          表示する候補のリスト
         """
-        del self[:num_cands]
-        return self
+        if len(self._lk) <= num_cands:
+            return self._lk
+        tmp = self._lk[:num_cands]
+        tmp.append("NEXT")
+        self._lk = self._lk[num_cands:]
+        return tmp
+
+    def select(self, num_cands, choice):
+        """
+        Parameters
+        ----------
+        num_cands : int
+          choice で確定しなかった場合の動作で，最大の選択肢個数を示す
+        choice : str
+          直前の選択肢郡から選んだ文字列
+
+        Returns
+        -------
+        Candidate or List[str]
+          確定したならばCandidateを，そうでなければList[str]で候補を提示する
+
+        Raises
+        -------
+        ValueError
+          if choice not in proposal
+        """
+        if choice == "NEXT":
+            return self.cands(num_cands)
+        if choice not in self._expects:
+            raise ValueError(f"{choice} is not in proposal")
+        return self._expects[choice]
 
     def __str__(self):
         cands = ", ".join([str(c) for c in self])
         return f"title: {self.title}, cands: [{cands}]"
+
+
+class KanaCandidates:
+    """
+    受け取ったcandsをカナによって事前振り分けした上でcandsから1つ選択する
+
+    Attributes
+    ----------
+    title : str
+    parent : Refiner
+    _cands : List[Candidate]
+    _last : List[str]
+
+    Examples
+    --------
+    国名の場合，国名をカナで書いたときの先頭N文字（1で足りる？）がまず選択肢となる．
+    カナを選択したのちに，そのカナから始まる国名一覧によって国名が選択可能となる．
+    """
+
+    def __init__(self, title, cands, parent):
+        """
+        Parameters
+        ----------
+        title : str
+        cands : List[Candidate] or List[str]
+        parent : Refiner
+
+        Raises
+        ------
+        ValueError
+          cands の表示値に被りがある場合
+        """
+        if type(cands[0]) is str:
+            cands = [Candidate(c) for c in cands]
+            # TODO 国名の被りを考える
+        # if not (len(cands) == len(set([c.key for c in cands]))):
+        #     raise ValueError("候補値に被りがあります．")
+        self.title = title
+        self.parent = parent
+        self._cands = cands
+        self._last = []
+        self._kana = ""
+        self._proposal = []
+        self._cands_dict = None
+
+    def cands(self, num_cands=30):
+        """
+        num_cands個以下の選択肢を返す．
+        0ならば全てを返す．
+
+        Parameters
+        ----------
+        num_cands : int
+
+        Returns
+        -------
+        List[str]
+          表示する候補のリスト
+        """
+        # 辞書がないなら作る
+        if self._cands_dict is None:
+            pl = [(c.ref, c.key) for c in self._cands]
+            # TODO 完了は辞書から外す or 最初に表示
+            self._cands_dict = {
+                kana: [Candidate(key=l, ref=s, kana=k) for (s, l, k) in val]
+                for kana, val in make_n_dict(pl, num_cands).items()
+            }
+        if self._last:
+            tmpl = self._last[:num_cands]
+            self._last = self._last[num_cands:]
+            if len(self._last) > 0:
+                tmpl.append("NEXT")
+            self._proposal = tmpl
+            return tmpl
+        elif len(self._cands_dict.keys()) <= num_cands:
+            self._proposal = list(self._cands_dict.keys())
+            return self._proposal
+        else:
+            tmpl = list(self._cands_dict.keys())
+            self._last = tmpl[num_cands:]
+            tmpl = tmpl[:num_cands]
+            tmpl.append("NEXT")
+            self._proposal = tmpl
+            return tmpl
+
+    def select(self, num_cands, choice):
+        """
+        Parameters
+        ----------
+        num_cands : int
+          choice で確定しなかった場合の動作で，最大の選択肢個数を示す
+        choice : str
+          直前の選択肢郡から選んだ文字列
+
+        Returns
+        -------
+        Candidate or List[str]
+          確定したならばCandidateを，そうでなければList[str]で候補を提示する
+
+        Raises
+        -------
+        ValueError
+          if choice not in proposal
+        """
+        if choice == "NEXT":
+            return self.cands(num_cands)
+        if choice not in self._proposal:
+            raise ValueError("not in proposal")
+        if self._kana == "":
+            self._kana = choice
+            self._proposal = OrderedDict([(c.key, c) for c in self._cands_dict[choice]])
+            return list(self._proposal.keys())
+        else:
+            return self._proposal[choice]
+
+    def __str__(self):
+        return super().__str__()
 
 
 class Priority(Enum):
